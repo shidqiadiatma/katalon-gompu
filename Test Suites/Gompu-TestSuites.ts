19a6deec46da32c8f09620fe20d69bfd9bd11a7a
hipstertester <shidqiadiatma12@gmail.com>
2023-04-01 20:46:18 +0700

    updated

--- Test Suites/Gompu-TestSuites.ts
+++ Test Suites/Gompu-TestSuites.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>New Test Suite</name>
+   <name>Gompu-TestSuites</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
